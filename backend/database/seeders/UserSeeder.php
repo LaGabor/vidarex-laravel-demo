b1ed6396dfde79a0c16a8f0a64752fae82442150
LaGabor <lagaborka@gmail.com>
2023-08-14 20:40:15 +0200

    feat:Device model,migration,seeder,factory and user seeder created

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create(
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('123'),
+                'role' => 1,
+            ]
+        );
+        User::create(
+            [
+                'name' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => bcrypt('123'),
+                'role' => 0,
+            ]
+        );
+    }
+}
