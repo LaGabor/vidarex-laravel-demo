0b078f3c1d36e1f4797762c202683b9e5d8ae9da
Gábor <lagaborka@gmail.com>
2023-08-21 17:53:00 +0200

    NEW BRANCH?

--- backend/app/Models/Device.php
+++ backend/app/Models/Device.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+// CHANGED!
 class Device extends Model
 {
     use HasFactory, SoftDeletes;
